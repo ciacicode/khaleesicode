0b2ccbb8acddb98a416024bab1e7ca2d7c82bb93
Grutin <m.gruau@gmail.com>
2015-03-07 12:59:45 +0000

    removed the need to downnload xml added zip code

--- xmlparser.py
+++ xmlparser.py
@@ -1,12 +1,15 @@
 import xml.etree.ElementTree as ET
-
+from urllib import urlopen
+from xml.etree.ElementTree import parse
 
 # parse the file with the etree library
-tree = ET.parse('/home/maria/Desktop/ciacicode/fci/data.xml')
+u = urlopen('http://data.gov.uk/data/resource_cache/be/be400256-25b3-4228-a556-3ffa557b4c18/FHRS524en-GB.xml')
+tree = ET.parse(u)
 root = tree.getroot()
 collection = root.find('EstablishmentCollection')
 
 for detail in collection.findall('EstablishmentDetail'):
     bizName = detail.find('BusinessName').text
+    postCode = detail.findtext('PostCode')
     rating = detail.find('RatingValue').text
-    print bizName, rating
+    print bizName, postCode, rating
