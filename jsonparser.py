642307b8f10a9cc8683d2c698ab439ba07735186
Maria Cerase <maria.cerase@gmail.com>
2015-03-08 22:19:22 +0000

    parsed json data to create a dictionary of xml files

--- jsonparser.py
+++ jsonparser.py
@@ -17,12 +17,28 @@ def decode (data):
 
 inputJson = decode(url)
 
-for item in inputJson:
-    if item == 'resources':
+for key in inputJson.keys():
+    if key == 'resources':
         # dive into the resources
         resourcesList = inputJson['resources']
         
+    '''resourcestList is a list of dictionaries each containing information
+    regarding the area that was inspected for hygiene, when it was updated
+    and where the xml file is tored'''
+    
+
+    '''The next part of the code will iterate over the resources to store
+    last_modified, description,url into a dict'''
+
+resourcesDict={}
 
 
-        
-        
+for entry in resourcesList:
+    nestDict = {}
+    nestDict['last_modified'] = entry['last_modified']
+    nestDict['url'] = entry['url']
+    resourcesDict[entry['description']] = nestDict
+    
+    
+    
+    
