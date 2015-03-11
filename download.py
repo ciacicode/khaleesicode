1221bc24d4f01107e0022f783f020e246c5b8b37
Maria <maria.cerase@gmail.com>
2015-03-11 22:09:49 +0000

    Delete download.py

--- download.py
+++
@@ -1,5 +0,0 @@
-import urllib
-
-# download and save data in a file
-url = "http://data.gov.uk/data/resource_cache/be/be400256-25b3-4228-a556-3ffa557b4c18/FHRS524en-GB.xml"
-urllib.urlretrieve (url,"/home/martin/fci/data.xml")
