86bdf560f4ba86f22797f3754a613241acf2c0af
maria <maria@privax.com>
2015-07-31 14:51:29 +0100

    fixed potential problem with sql schema file location

--- khaleesicode.py
+++ khaleesicode.py
@@ -34,7 +34,7 @@ def connect_db():
 
 def init_db():
     with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
+        with app.open_resource('static/schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
