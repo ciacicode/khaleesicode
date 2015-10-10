a44e44364a011d7e90fbe0a240d8ebf70a68b045
Maria Cerase <maria.cerase@gmail.com>
2015-10-10 17:11:11 +0100

    added requirements.txt file and added fancy code highlight for blog

--- mysite/modules/charts.py
+++ mysite/modules/charts.py
@@ -22,15 +22,16 @@ london_p = lp.data.copy()
 postcode_xs = [london_p[name]["lons"] for name in london_p]
 postcode_ys = [london_p[name]["lats"] for name in london_p]
 
-colors = [ "#eaf9fd", "#c1edfa", "#99e1f7", "#70d5f4", "#47c9f1","#33c3f0","#2dafd8", "#2388a8", "#196178", "#0f3a48", "#0a2730" ]
+colors = ["#eaf9fd", "#c1edfa", "#99e1f7", "#70d5f4", "#47c9f1", "#33c3f0", "#2dafd8", "#2388a8", "#196178", "#0f3a48",
+          "#0a2730"]
 
 postcode_name = []
 postcode_colours = []
 postcode_fci = []
 
 for name in london_p:
+    parent = name[0]
     try:
-        parent = name[0]
         fci_value = float(fci_data[parent])
         idx = int(fci_value/10)
         postcode_colours.append(colors[idx])
@@ -43,16 +44,17 @@ for name in london_p:
         postcode_fci.append(fci_value)
 
 
-source = ColumnDataSource( data = dict(x=postcode_xs, y=postcode_ys, color=postcode_colours, name=postcode_name,rate=postcode_fci,))
+source = ColumnDataSource(data=dict(x=postcode_xs, y=postcode_ys, color=postcode_colours, name=postcode_name,
+                                    rate=postcode_fci,))
 
 TOOLS = "pan, wheel_zoom, box_zoom, reset, hover, save"
 
-p = figure(title="", toolbar_location="left", plot_width=800, plot_height=450, tools = TOOLS)
+p = figure(title="", toolbar_location="left", plot_width=800, plot_height=450, tools=TOOLS)
 
 p.patches('x', 'y', fill_color='color', fill_alpha=0.7, line_color="#884444", line_width=0.5, source=source)
 
 hover = p.select(dict(type=HoverTool))
 hover.point_policy = "follow_mouse"
-hover.tooltips = OrderedDict([("Name","@name"),("FCI","@rate"),("Long, Lat","$x, $y"),])
+hover.tooltips = OrderedDict([("Name", "@name"), ("FCI", "@rate"), ("Long, Lat", "$x, $y"), ])
 
 script, div = components(p)
