6849ee6ac37a837981dadfe4e71b8216b9511ee5
Maria Cerase <maria.cerase@gmail.com>
2015-12-29 10:41:45 +0000

    started on unit tests and also removed legacy sql schema

--- mysite/modules/tests.py
+++ mysite/modules/tests.py
@@ -1,6 +1,20 @@
 __author__ = 'ciacicode'
 import os
-from mysite import api
-from mysite import site_views
+from mysite import app
 import unittest
 import tempfile
+
+class TestDb(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        flaskr.app.config['TESTING'] = True
+        self.app = flaskr.app.test_client()
+        flaskr.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(flaskr.app.config['DATABASE'])
+
+if __name__ == '__main__':
+    unittest.main()
